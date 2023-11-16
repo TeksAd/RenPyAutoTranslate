d1af8ed5ae2cab0ff6f76818cdb83d5000a62107
TeksAd <150190854+TeksAd@users.noreply.github.com>
2023-11-16 18:05:46 +0000

    Update main.py
    Усложнён парсинг, чтобы потом когда-нибудь добавить новые фичи. Вместе с этим поломан парсинг строк содержащих " типа " бла-бла-бла \"бла-бла-бла\" "

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import argostranslate.package
 import argostranslate.translate
-from pprint import pprint
 import os
+from pprint import pprint
 
 
 class Translator:
-    def __init__(self, from_code: str, to_code: str):
+    """
+    Обёртка для argostranslate
+    """
+    def __init__(self, from_code: str, to_code: str, file_code: str = ""):
+        self.file_code = file_code
         self.from_code = from_code
         self.to_code = to_code
         self.path = self.download_translate_tackage(from_code, to_code)
@@ -27,67 +31,181 @@ class Translator:
         print("Языковой пакет скачан в файл: ", path)
         return path
 
-    def translate(self, text_to_translate: str):
+    def translate(self, text_to_translate: str) -> str:
+        '''
+        Принимает строку, которую нужно перевести, возвращает переведённую строку, в начале которой стоит
+        id файла (передаётся в конструкторе) символ @ и номер строки в файле
+        т.е. id@НомерСтроки ПереведённаяСтрока
+        :param text_to_translate:
+        :return:
+        '''
         self.count += 1
-        return ("@" +
+        return (self.file_code +
+                "@" +
                 str(self.count) +
                 " " +
                 argostranslate.translate.translate(text_to_translate, self.from_code, self.to_code)
                 )
 
 
-tr = Translator("en", "ru")
+class Phrase:
+    def __init__(self, line: str):
+        line = line[4:]
+        if line[-2] == '"':
+            self.after = ""
+        else:
+            self.after = line[line.rfind('"')+1:-1]
+            line = line[:line.rfind('"')+1]
+        if line[:3] == "old":
+            self.speaker = "old"
+            self.is_system = True
+            self.speech = line[5:-2]
+        elif line[:3] == '# "':
+            if line[3:-2].find('"') == -1:# or line[3:-2].find('"') != line[3:-2].find('\\"'):
+                self.speaker = None
+                self.speech = line[1:-2]
+                self.is_system = False
+            else:
+                line = line[2:]
+                line.find('"',1)
+                self.speaker = line[:line.find('"',1)+1]
+                self.speech = line[line.find('"',1)+2:-2]
+                self.is_system = False
+        else:
+            line = line[2:]
+            self.speaker, self.speech = line.split(" ", 1)
+            self.speech = self.speech[1:-1]
+            self.is_system = False
+
+    def __str__(self):
+        return f"-\nspeaker: {self.speaker} \nspeech: {self.speech} \nafter: {self.after}"
 
 
-def get_files(top="."):
+class Block:
+    def __init__(self):
+        self.before_lines = []
+        self.old_line = None
+        self.new_line = None
+        self.repeats = []
+
+    def append_before_line(self, line):
+        self.before_lines.append(line)
+
+    def set_old_line(self, line):
+        self.old_line = Phrase(line)
+
+    def append_repeat(self, repeat):
+        self.repeats.append(repeat)
+
+
+class File:
+    def __init__(self, lines, tr):
+        self.blocks = []
+        self.tr = tr
+
+        bl = Block()
+        i = 0
+        l = len(lines)
+        while i < l:
+            if lines[i][:4] != "    " or (lines[i][:5] == "    #" and ".rpy" in lines[i]):
+                bl.append_before_line(lines[i])
+            else:
+                bl.append_before_line(lines[i])
+                bl.set_old_line(lines[i])
+                self.blocks.append(bl)
+                bl = Block()
+                i += 2
+            i += 1
+
+    def translate(self):
+        for i in self.blocks:
+            i.new_line = "    "
+            if i.old_line.is_system:
+                i.new_line += "new "
+            elif i.old_line.speaker is not None:
+                i.new_line += i.old_line.speaker + " "
+            i.new_line += '"'
+            i.new_line += self.tr(i.old_line.speech)
+            # print(i.old_line)
+            i.new_line += '"'
+            i.new_line += i.old_line.after
+            i.new_line += '\n'
+
+    def get_lines(self):
+        result = []
+        for i in self.blocks:
+            for j in i.before_lines:
+                result.append(j)
+            result.append(i.new_line)
+            result.append("\n")
+        return result
+
+
+def get_rpy_files(top=".", quiet = False):
     files = []
     for rt, ds, fs in os.walk(top):
         for i in fs:
             if i[-4:] == ".rpy":
                 file = os.path.join(rt, i)
-                print(file)
+                if not quiet:
+                    print(file)
                 files.append(file)
     return files
 
 
-def parse(row: str) -> str:
-    parts = row.split(sep='"', maxsplit=1)
-    parts += (parts.pop().rsplit(sep='"', maxsplit=1))
-    if parts[0][4:7] == "old":
-        parts[0] = parts[0][:4] + "new" + parts[0][7:]
-
-    parts[1] = tr.translate(parts[1])
-    return parts[0] + '"' + parts[1] + '"' + parts[2]
-
-
-def edit(file: str):
-    result = []
+def read_file(file: str):
     with open(file, "r", encoding='UTF8') as f:
-        rows = f.readlines()
+        result = f.readlines()
+    return result
 
-    for row in rows:
-        # pprint("="*80)
-        # pprint(row)
-        if row[0:4] != "    " or row[0:5] == "    #" or row[0:8] == "    old ":
-            result.append(row)
-            # pprint(row)
-        else:
-            result.append(parse(result[-1]))
-            # pprint(result[-1])
-    pprint(result)
+
+def write_file(file: str, lines):
     with open(file, "w", encoding='UTF8') as f:
-        f.writelines(result)
+        f.writelines(lines)
 
 
-def main():
-    # tr = translator("en", "ru")
-    # print(tr.translate("Hello World"))
-
-    files = get_files()
-
-    edit(files[0])
-    # for i in files:
-    #     edit(i)
+def placeholertranslate(s: str):
+    return ""
 
 
-main()
+def test_file(file):
+    test = read_file(file)
+    f = File(test, placeholertranslate)
+    f.translate()
+    res = f.get_lines()
+    if res != test:
+        print(file)
+        # print(len(res) == len(test))
+        for j in range(len(res)):
+            if res[j] != test[j]:
+                print(f'Строка {j}:')
+                pprint(test[j])
+                pprint(res[j])
+
+
+def test_all_files():
+    for i in get_rpy_files(quiet=True):
+        test_file(i)
+
+
+def test_piece_of_file(file, begin, end):
+    t = []
+    test = read_file(file)[begin: end]
+    f = File(test, "tr")
+    f.translate()
+    res = f.get_lines()
+    for i in range(end-begin):
+        print(f'Строка {i}:')
+        pprint(test[i])
+        pprint(res[i])
+        t.append(test[i])
+    return t
+
+
+if __name__ == '__main__':
+    # test_all_files()
+    translator = Translator("en", "ru", "common.rpy")
+    f = File(read_file("common.rpy"), translator.translate)
+    f.translate()
+    lines = f.get_lines()
+    write_file("common.rpy", lines)
